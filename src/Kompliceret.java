98bab42824625bcda190b274c6aad6645c7536b0
András Ács <acsandras@gmail.com>
2020-03-19 13:22:29 +0100

    Små fix

--- src/Kompliceret.java
+++ src/Kompliceret.java
@@ -1,24 +1,24 @@
+import static java.lang.Math.pow;
+
 public class Kompliceret {
 
     public static void main(String[] args) {
 
         MyTimer.start();
 
-        int n = 100000;
+        int n = 30;
 
         long resultat = 0;
 
         // Denne algoritme tager meget lang tid at gennemføre, og har en algoritmisk kompleksitet på O(N^2)
-        for (int i = 0; i < n; i++) {
-            if (i%10000==0) System.out.print(".");
-            for (int j = 0; j < i; j++) {
-                for (int k = 0; k < j; k++) {
-                     resultat = i*j*k;
-                }
-
+        for (int i = 1; i < n; i++) {
+            System.out.print(".");
+            for (int j = 1; j <= Math.pow(2, i); j++) {
+                     resultat += i+j;
             }
 
         }
+        
         System.out.println("Resultat " + resultat);
         MyTimer.stop();
 
